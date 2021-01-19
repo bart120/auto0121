907edbf9709ef640194fdb7e775d7f94ef1edc0e
Vincent <vleclerc@inow.fr>
2021-01-19 14:10:44 +0100

    template, events

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared.module';
 
 const childRoutes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -12,7 +13,8 @@ const childRoutes: Routes = [
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(childRoutes)
+    RouterModule.forChild(childRoutes),
+    SharedModule
   ]
 })
 export class AuthenticationModule { }
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user: any = null;
+
+  @ViewChild('password')
+  passwd: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(mail: string): void {
+    //console.log(mail);
+    //console.log(this.passwd.nativeElement.value);
+    this.user = { login: mail, name: 'Bob' };
+  }
+
+  onLogout(): void {
+    this.user = null;
   }
 
 }
