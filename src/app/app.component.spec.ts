1ed5a5d59387ad8efa1b4cad3802dd6f7e78b4c5
Vincent <vleclerc@inow.fr>
2021-01-20 17:38:44 +0100

    tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('auto0121 app is running!');
+    expect(compiled.querySelector('.bob span').textContent).toContain('auto0121');
   });
 });
